ee6840a9fb72ef2f8697899b21685e478dd62609
eduardocd18 <javierecd@gmail.com>
2024-03-24 12:28:37 -0400

    181 - Video en HTML 5

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
 
+/* -- Función para compilar el CSS-- */
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo de sass
     .pipe(plumber())
@@ -11,11 +12,12 @@ function css(done) {
   done();
 }
 
+/* -- Función para llamar el compilado del CSS -- */
 function dev(done) {
   watch("src/scss/**/*.scss", css);
   done();
 }
 
-// Exportaciones o tareas ejecutables
+/* -- Exportaciones o tareas ejecutables -- */
 exports.css = css;
 exports.dev = dev;
