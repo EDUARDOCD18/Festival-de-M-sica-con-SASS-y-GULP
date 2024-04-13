381f78cfe7d6f227361d5a2ec1c20d3150d3bd1a
eduardocd18 <javierecd@gmail.com>
2024-04-13 11:47:48 -0400

    200. Añadir un botón para cerrar la imagen

--- build/js/app.js
+++ build/js/app.js
@@ -27,10 +27,29 @@ function mostrarImagen(id) {
   imagen.innerHTML = `<source srcset="build/img/grande/${id}.avif" type="image/avif" />
     <source srcset="build/img/grande/${id}.webp" type="image/webp" /><img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galería"/>`;
 
+  /* -- Crea el overlay con la imagen */
   const overlay = document.createElement("DIV");
   overlay.appendChild(imagen);
   overlay.classList.add("overlay");
+  overlay.onclick = function () {
+    body.appendChild(overlay);
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
 
+  /* -- Botón para cerrar el modal */
+  const cerrarModal = document.createElement("P");
+  cerrarModal.textContent = "X";
+  cerrarModal.classList.add("btn-cerrar");
+  cerrarModal.onclick = function () {
+    body.appendChild(overlay);
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
+  overlay.appendChild(cerrarModal);
+
+  /* -- Añadir al HTML -- */
   const body = document.querySelector("body");
   body.appendChild(overlay);
+  body.classList.add("fijar-body");
 }
--- src/js/app.js
+++ src/js/app.js
@@ -27,10 +27,34 @@ function mostrarImagen(id) {
   imagen.innerHTML = `<source srcset="build/img/grande/${id}.avif" type="image/avif" />
     <source srcset="build/img/grande/${id}.webp" type="image/webp" /><img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galería"/>`;
 
+  /* -- Crea el overlay con la imagen */
   const overlay = document.createElement("DIV");
   overlay.appendChild(imagen);
   overlay.classList.add("overlay");
 
+  /* -- Función para cerrar el overlay tocando en cualquier parte del mismo */
+  overlay.onclick = function () {
+    body.appendChild(overlay); // Agrega overlay al body
+    body.classList.remove("fijar-body"); // Quita clase al body
+    overlay.remove(); // Quita el overlay
+  };
+
+  /* -- Botón para cerrar el modal */
+  const cerrarModal = document.createElement("P");
+  cerrarModal.textContent = "X";
+  cerrarModal.classList.add("btn-cerrar");
+
+  /* -- Función para cerrar el overlay haciendo click en el botón cerrar */
+  cerrarModal.onclick = function () {
+    body.appendChild(overlay); // Agrega overlay al body
+    body.classList.remove("fijar-body"); // Quita clase al body
+    overlay.remove(); // Quita el overlay
+  };
+  
+  overlay.appendChild(cerrarModal);
+
+  /* -- Añadir al HTML -- */
   const body = document.querySelector("body");
   body.appendChild(overlay);
+  body.classList.add("fijar-body");
 }
